0a08e088114debfd39a6aa664a4771a7c27346cf
ShahzaibAhmad05 <shahzaibahmad6789@gmail.com>
2026-01-04 20:21:28 +0500

    Implement app context in top-level functions

--- gitree/objects/app_context.py
+++ gitree/objects/app_context.py
@@ -7,14 +7,12 @@ Helps avoid passing hundreds of args to functions and using global vars just
 for the app context. 
 """
 
-# Default libs
-from dataclasses import dataclass
-
 # Deps in the same project
 from ..utilities.logger import Logger, OutputBuffer
 
 
-@dataclass
 class AppContext:
-    logger: Logger
-    output: OutputBuffer
+    def __init__(self) -> None:
+        """ Constructor for app ctx """
+        self.logger = Logger()
+        self.output_buffer = OutputBuffer()
