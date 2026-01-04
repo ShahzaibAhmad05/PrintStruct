9da6e37d062ed3903e462086444039cea2383c68
ShahzaibAhmad05 <shahzaibahmad6789@gmail.com>
2026-01-04 19:11:45 +0500

    Add doc details to app_context

--- gitree/objects/app_context.py
+++ gitree/objects/app_context.py
@@ -1,5 +1,17 @@
-from ..utilities.logger import Logger, OutputBuffer
+# gitree/objects/app_context.py
+
+"""
+Context object for the app.
+
+Helps avoid passing hundreds of args to functions and using global vars just 
+for the app context. 
+"""
+
+# Default libs
 from dataclasses import dataclass
+
+# Deps in the same project
+from ..utilities.logger import Logger, OutputBuffer
 
 
 @dataclass
